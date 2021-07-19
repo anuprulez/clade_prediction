0b0085576423a597045f25185bde9788bad9ca13
Anup Kumar <anup.rulez@gmail.com>
2021-07-19 17:13:11 +0200

    Update main

--- main.py
+++ main.py
@@ -25,13 +25,14 @@ PATH_SEQ_CLADE = PATH_PRE + "ncov_global.tsv"
 PATH_CLADES = "data/clade_in_clade_out.json"
 KMER_SIZE = 3
 EMBED_DIM = 4
-BATCH_SIZE = 16
+batch_size = 16
+units = 16
 
 # https://www.tensorflow.org/text/tutorials/nmt_with_attention
 
 
 def read_files():
-    '''samples_clades = preprocess_sequences.get_samples_clades(PATH_SEQ_CLADE)
+    samples_clades = preprocess_sequences.get_samples_clades(PATH_SEQ_CLADE)
     
     clades_in_clades_out = utils.read_json(PATH_CLADES)
 
@@ -54,10 +55,10 @@ def read_files():
 
     train_x = [list(map(int, lst)) for lst in train_x]
     train_y = [list(map(int, lst)) for lst in train_y]
-    '''
+    
     print("Creating neural network...")
     
-    embedding_dim = 4
+    '''embedding_dim = 4
     units = 16
     batch_size = 8
     seq_len = 50
@@ -69,12 +70,12 @@ def read_files():
     
     sample_output = [np.random.randint(vocab_size, size=seq_len) for i in range(3 * batch_size)]
     sample_output = np.array(sample_output)
-    print(sample_output.shape)
+    print(sample_output.shape)'''
     
-    dataset_in = tf.data.Dataset.from_tensor_slices((sample_input)).batch(batch_size)
-    dataset_out = tf.data.Dataset.from_tensor_slices((sample_output)).batch(batch_size)
+    dataset_in = tf.data.Dataset.from_tensor_slices((train_x)).batch(batch_size)
+    dataset_out = tf.data.Dataset.from_tensor_slices((train_y)).batch(batch_size)
 
-    start_training(dataset_in, dataset_out, embedding_dim, units, batch_size, seq_len, vocab_size)
+    #start_training(dataset_in, dataset_out, embedding_dim, units, batch_size, seq_len, vocab_size)
 
 def start_training(input_batch, output_batch, embedding_dim, units, batch_size, seq_len, vocab_size):
 
